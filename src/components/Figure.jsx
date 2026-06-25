ebdd401dc1dc6dc90a281a61fe828a4389c9bfbd
chittybb1357-commits <chittybb1357@gmail.com>
2026-06-25 17:23:21 +0900

    feat: add Figure component to display image and description

--- src/components/Figure.jsx
+++ src/components/Figure.jsx
@@ -1,1 +1,12 @@
+function Figure({ data }) {
+  const { url, desc } = data;
+
+  return (
+    <figure>
+      <img alt={desc} src={url} />
+      <figcaption>{desc}</figcaption>
+    </figure>
+  );
+}
+
 export default Figure;
