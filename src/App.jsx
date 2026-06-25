49f58411644caa679868553a626e0949d32c6230
chittybb1357-commits <chittybb1357@gmail.com>
2026-06-25 17:17:41 +0900

    feat: add works data and component imports in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Figure from "./components/Figure";
 
 function App() {
+  const works = [
+    { id: 1, title: "work 1", url: "images/img-1.jpg", desc: "work 1 description" },
+    { id: 2, title: "work 2", url: "images/img-2.jpg", desc: "work 2 description" },
+    { id: 3, title: "work 3", url: "images/img-3.jpg", desc: "work 3 description" },
+    { id: 4, title: "work 4", url: "images/img-4.jpg", desc: "work 4 description" },
+  ];
   return <></>;
 }
 export default App;
